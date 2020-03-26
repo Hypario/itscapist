8e642c3d7826b0ab4db4717efedc01fe310fe486
Amaury Hubert <amaury_hubert@ens.univ-artois.fr>
2020-03-26 21:28:28 +0100

    animations du personnage principal

--- itscapist-angular/src/app/app-routing.module.ts
+++ itscapist-angular/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import {GameComponent} from './game/game.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'profile', component: ProfileComponent}
+  {path: 'profile', component: ProfileComponent},
+  {path: 'jeu', component: GameComponent}
 ];
 
 @NgModule({
