c60fecb46bd3032e8f3e921a7d5323c3e976a4bb
Thomas <tdignoire@protonmail.com>
2020-03-27 14:46:41 +0100

    Added maps source files & items pritesheet

---
+++ itscapist-angular/src/assets/maps/tiles/Itscapist_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="Itscapist_tiles" tilewidth="16" tileheight="16" tilecount="100" columns="4">
+ <image source="../../../../../../../../../maps/tiles/tiles_cus_perks.gif" trans="ffffff" width="64" height="400"/>
+</tileset>
